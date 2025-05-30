c7181db33db3afe915293a7edde0634e2ba14000
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-05-30 10:24:54 +0530

    done bro

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const e = require('express')(),
-  p = require('path');
-e.get('/:f', (q, s) => s.sendFile(p.join(__dirname, q.params.f + '.txt')));
+  p = require('path'),
+  fs = require('fs');
+e.get('/:f', (q, s) => {
+  try {
+    let f = p.join(__dirname, q.params.f + '.txt');
+    fs.access(f, fs.constants.F_OK, (err) =>
+      err ? s.status(404).send('Not Found') : s.sendFile(f)
+    );
+  } catch (x) {
+    s.status(500).send('Server Error');
+  }
+});
 e.listen(process.env.PORT || 3000);
-    
